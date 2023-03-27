b7c866050f661bb659da706cb7e193b5a1abd8a0
Shreyos Ghosh <shreyosghosh279@gmail.com>
2023-03-28 01:31:15 +0530

    Added material system

--- Raether/src/Scene.h
+++ Raether/src/Scene.h
@@ -5,23 +5,30 @@
 
 #include <glm\glm.hpp>
 
-struct Materials {
-
+struct Material {
+	glm::vec3 Albedo{ 1.0f };
+	float Roughness{ 0.1f };
+	float Metallic{ 0.0 };
 };
 
 struct Sphere {
-	glm::vec3 sphereOrigin{ 0.0f };
-	float radius = 0.5f;
-	glm::vec3 albedo{ 1.0f };
+	glm::vec3 SphereOrigin{ 0.0f };
+	float Radius = 0.5f;
+
+	int MatIndex = 0;
 };
 
 struct Scene {
 	glm::vec3 Lightdirection;
 
 	std::vector<Sphere> SphereList;
+	std::vector<Material> Materials;
 
 	void addSpheres(Sphere sphere) {
 		SphereList.push_back(sphere);
+	}
+	void addMaterials(Material mat) {
+		Materials.push_back(mat);
 	}
 };
 
@@ -29,6 +36,6 @@ struct Hitrec {
 	glm::vec3 Hitpoint;
 	glm::vec3 Surfacenormal;
 	glm::vec3 HitColor;
-	glm::vec3 Hitobjorigin;
+	int Hitobjindex;
 };
 
