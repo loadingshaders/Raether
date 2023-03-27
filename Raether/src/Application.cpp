fa111659891cb0065f5b93d83c699d99b6225766
Shreyos Ghosh <shreyosghosh279@gmail.com>
2023-03-28 01:35:14 +0530

    Added materials

--- Raether/src/Application.cpp
+++ Raether/src/Application.cpp
@@ -16,48 +16,77 @@ void App::setUpScene() {
 	// Window Setup
 	setWindow();
 
+	// Configure Materials
+	{
+		Material redSpheres;
+		redSpheres.Albedo = glm::vec3(1.0f, 0.0f, 0.0f);
+		redSpheres.Metallic = 0.0f;
+		redSpheres.Roughness = 0.0f;
+
+		scene.addMaterials(redSpheres);
+	}
+	{
+		Material greenSpheres;
+		greenSpheres.Albedo = glm::vec3(0.0f, 1.0f, 0.0f);
+		greenSpheres.Metallic = 0.0f;
+		greenSpheres.Roughness = 0.2f;
+
+		scene.addMaterials(greenSpheres);
+	}
+	{
+		Material blueSpheres;
+		blueSpheres.Albedo = glm::vec3(0.0f, 0.0f, 1.0f);
+		blueSpheres.Metallic = 0.0f;
+		blueSpheres.Roughness = 0.5f;
+
+		scene.addMaterials(blueSpheres);
+	}
+
 	// Configure spehers
-	{
-		sphere.sphereOrigin = glm::vec3(-1.0f, 0.0f, 0.0f);
-		sphere.radius = 0.5f;
-		sphere.albedo = glm::vec3(1.0f, 0.0f, 0.0f);
-		// Add the Sphere to the scene
-		scene.addSpheres(sphere);
-	}
-	{
-		sphere.sphereOrigin = glm::vec3(1.0f, 0.0f, 0.0f);
-		sphere.radius = 0.5f;
-		sphere.albedo = glm::vec3(0.0f, 0.0f, 1.0f);
-		// Add the Sphere to the scene
-		scene.addSpheres(sphere);
-	}
-	{
-		sphere.sphereOrigin = glm::vec3(0.0f, 0.0f, 0.0f);
-		sphere.radius = 0.5f;
-		sphere.albedo = glm::vec3(0.0f, 1.0f, 0.0f);
-		// Add the Sphere to the scene
-		scene.addSpheres(sphere);
-	}
+	//{
+	//	sphere.SphereOrigin = glm::vec3(-1.0f, 0.0f, 0.0f);
+	//	sphere.Radius = 0.5f;
+	//	sphere.MatIndex = 0;
+
+	//	// Add the Sphere to the scene
+	//	scene.addSpheres(sphere);
+	//}
+	//{
+	//	sphere.SphereOrigin = glm::vec3(0.0f, 0.0f, 0.0f);
+	//	sphere.Radius = 0.5f;
+	//	sphere.MatIndex = 1;
+
+	//	// Add the Sphere to the scene
+	//	scene.addSpheres(sphere);
+	//}
+	//{
+	//	sphere.SphereOrigin = glm::vec3(1.0f, 0.0f, 0.0f);
+	//	sphere.Radius = 0.5f;
+	//	sphere.MatIndex = 2;
+
+	//	// Add the Sphere to the scene
+	//	scene.addSpheres(sphere);
+	//}
 
 	 // Configure spehers
-	//{
-	//	sphere.sphereOrigin = glm::vec3(0.0f, -30.5f, 0.0f);
-	//	sphere.radius = 30.0f;
-	//	sphere.albedo = glm::vec3(1.0f, 1.0f, 1.0f);
-	//	// Add the Sphere to the scene
-	//	scene.addSpheres(sphere);
-	//}
-	//{
-	//	sphere.sphereOrigin = glm::vec3(0.0f, 0.0f, 0.0f);
-	//	sphere.radius = 0.5f;
-	//	sphere.albedo = glm::vec3(1.0f, 1.0f, 1.0f);
-	//	// Add the Sphere to the scene
-	//	scene.addSpheres(sphere);
-	//}
+	{
+		sphere.SphereOrigin = glm::vec3(0.0f, 0.0f, 0.0f);
+		sphere.Radius = 0.5f;
+		sphere.MatIndex = 0;
+
+		scene.addSpheres(sphere);
+	}
+	{
+		sphere.SphereOrigin = glm::vec3(0.0f, -30.5f, 0.0f);
+		sphere.Radius = 30.0f;
+		sphere.MatIndex = 1;
+
+		scene.addSpheres(sphere);
+	}
 
 	// Camera setup
-	camera.SetPosition(glm::vec3(0.0f, 0.5f, 2.0f)); // 0.0f, 1.0f, 2.0f
-	camera.SetOrientation(glm::vec3(0.0f, -0.3f, -1.2f)); // 0.0f, -0.5f, -1.0f
+	camera.SetPosition(glm::vec3(0.0f, 0.5f, 2.3f)); // 0.0f, 1.0f, 2.0f
+	camera.SetOrientation(glm::vec3(0.0f, -0.30f, -1.2f)); // 0.0f, -0.5f, -1.0f
 	camera.SetViewPortWidth(width);
 	camera.SetViewPortHeight(height);
 	camera.SetProjection(45.0f, 0.1f, 100.0f);
@@ -65,7 +94,7 @@ void App::setUpScene() {
 	camera.CalculateRayDirections();
 
 	// Adding the light direction to the scene
-	scene.Lightdirection = glm::normalize(glm::vec3(0.0f, -1.0f, 0.0f));
+	scene.Lightdirection = glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
 }
 void App::run() {
 	setUpScene();
