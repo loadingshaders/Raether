01481f47582200808e291f0de960a6942a38dd10
Shreyos Ghosh <shreyosghosh279@gmail.com>
2023-07-18 04:34:22 +0530

    Added 'SCROLLING' to mousestate to handle FOV change

--- Raether/src/Raether.h
+++ Raether/src/Raether.h
@@ -13,7 +13,7 @@ namespace glm {
 
 enum struct RaeState { ACTIVE, EXIT };
 enum struct Keystate { STANDBY, W, A, S, D, Q, E};
-enum struct Mousestate { STATIC, INMOTION };
+enum struct Mousestate { STATIC, INMOTION , SCROLLING};
 
 class Raether {
 public:
@@ -21,17 +21,17 @@ public:
 	~Raether();
 
 	void raeCreateWindow(const char* w_t, int w_width, int w_height);
-	void raeRun();
 	void raeDrawPix(int u, int v, std::vector<glm::ui8_tvec4>& PixData);
 	void raeRenderBegin();
 	void raeRenderEnd();
-	bool raeIP();
+	bool raeInputEvents();
 	void raeQuit();
 
 	RaeState windowState;
 	Keystate keyState;
 	Mousestate mouseState;
 	glm::vec2 delta;
+	Sint32 scrollAmount;
 private:
 	SDL_Window* window;
 	const char* title;
