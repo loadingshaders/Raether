44ece5898cc4723b2c887229287f95318dfd5ac4
Shreyos Ghosh <shreyosghosh279@gmail.com>
2023-07-18 04:43:14 +0530

    Implemented emissive materials

--- Raether/src/Renderer.cpp
+++ Raether/src/Renderer.cpp
@@ -71,12 +71,6 @@ namespace Utils {
 	}
 }
 
-void Renderer::Init(Raether& rae, const Scene& scene, Camera& camera) {
-	raeObj = &rae;
-	renderCam = &camera;
-	renderScene = &scene;
-}
-
 glm::ui8_tvec4 Renderer::GammaCorrection(glm::vec4 color) {
 
 	glm::vec4 c{0.0f};
@@ -88,6 +82,12 @@ glm::ui8_tvec4 Renderer::GammaCorrection(glm::vec4 color) {
 	c.b = glm::sqrt(c.b * scale);
 
 	return color;
+}
+
+void Renderer::Init(Raether& rae, const Scene& scene, Camera& camera) {
+	raeObj = &rae;
+	renderCam = &camera;
+	renderScene = &scene;
 }
 
 void Renderer::Render(const Scene& scene, Camera& camera) {
@@ -114,10 +114,10 @@ void Renderer::Render(const Scene& scene, Camera& camera) {
 				AccumPixelData[x + y * width] += color;
 
 				glm::vec4 accumColor = AccumPixelData[x + y * width];
+
 				/// Balancing the brightness 
 				accumColor /= (float)FrameCount;
 
-				/// Check Whether it hits something or not and return a color according to it
 				accumColor = glm::clamp(accumColor, glm::vec4(0.0f), glm::vec4(1.0f));
 
 				/// Store the color data
@@ -126,9 +126,8 @@ void Renderer::Render(const Scene& scene, Camera& camera) {
 		}
 		/// Draw the color using SDL
 		raeObj->raeDrawPix(width, height, PixelData);
-
 	}
-	if (FrameCount <= 1000) {
+	if (FrameCount <= renderScene->SampleCount) {
 		FrameCount++;
 	}
 }
@@ -141,46 +140,33 @@ glm::vec4 Renderer::PerPixel(int x, int y) {
 
 	Hitrec hitrecord;
 
-	// Background color RGBa components
-	glm::vec4 background = black;
+	// Initialize the rayColor as white background
+	glm::vec4 rayColor = glm::vec4(white, 1.0f);
 
-	// Initialize the fragColor as black background
-	glm::vec4 fragColor = background;
-
-	float diffused;
-	float ambient = 0.4f;
-	glm::vec3 lightcolor{ 1.0f };
-	float attenuation = 1.0f;
+	glm::vec4 incomingLight = glm::vec4(black, 1.0f);
 
 	for (int i = 0; i < renderScene->Bounces; i++) {
+
 		if (Hittable(ray, renderScene->SphereList, hitrecord) == false) {
 
-			glm::vec4 SkyColor = Utils::Lerp(ray, white, blue);
+			/*glm::vec4 SkyColor = Utils::Lerp(ray, white, blue);
+			incomingLight += SkyColor * rayColor;*/
 
-			fragColor += SkyColor * attenuation;
-
-			return fragColor;
+			break;
 		}
 
 		const Sphere& sphere = renderScene->SphereList[hitrecord.Hitobjindex];
 		const Material& mat = renderScene->Materials[sphere.MatIndex];
 
-		glm::vec3 Tolight(-renderScene->Lightdirection);
-		diffused = glm::max(glm::dot(hitrecord.Surfacenormal, Tolight), 0.0f);
-
-		glm::vec3 ambientcolor = mat.Albedo * ambient;
-		glm::vec3 diffusedcolor = mat.Albedo * diffused * lightcolor;
-
-
-		fragColor += glm::vec4(ambientcolor + diffusedcolor, 1.0f) * attenuation;
-
-		attenuation *= 0.4f; // 0.5f
+		glm::vec3 emittedLight = mat.EmissionColor * mat.EmissionStrength;
+		incomingLight += glm::vec4(emittedLight, 1.0f) * rayColor;
+		rayColor = rayColor * glm::vec4(mat.Albedo, 1.0f);
 
 		ray.Origin = hitrecord.Hitpoint + hitrecord.Surfacenormal * 0.0001f;
 		ray.Direction = glm::reflect(ray.Direction + mat.Roughness * Utils::Randomoffset2(-1.0f, 1.0f), hitrecord.Surfacenormal);
 	}
 	 
-	return fragColor;
+	return incomingLight;
 }
 
 bool Renderer::Hittable(const Ray& ray, const std::vector<Sphere>& SphereList, Hitrec& hitrecord) {
@@ -245,7 +231,7 @@ bool Renderer::Hittable(const Ray& ray, const std::vector<Sphere>& SphereList, H
 		hitpoint = origin + ray.Direction * closestHit; // px = camera.x + direction.x * nt; py = camera.y + direction.y * nt; pz = camera.z + direction.z * nt;
 		hitrecord.Hitpoint = hitpoint;
 		hitrecord.Surfacenormal = glm::normalize(hitpoint);
-		hitrecord.Hitpoint = hitrecord.Hitpoint + SphereList[closestSphereIDX].SphereOrigin;
+		hitrecord.Hitpoint += SphereList[closestSphereIDX].SphereOrigin;
 		hitrecord.Hitobjindex = closestSphereIDX;
 
 		return true;
@@ -254,3 +240,96 @@ bool Renderer::Hittable(const Ray& ray, const std::vector<Sphere>& SphereList, H
 		return false;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+#define logtime std::chrono::steady_clock::now()
+#define elapsed std::chrono::duration_cast<std::chrono::milliseconds>
+#define clock std::chrono::steady_clock
+// 
+//Catch the start time
+clock::time_point start = logtime;
+// 
+//Catch the end time
+clock::time_point end = clock::now();
+printf("Last Frame Renderer => %d\n", elapsed(end - start).count());
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+glm::vec4 Renderer::PerPixel(int x, int y) {
+
+	Ray ray;
+	ray.Origin = renderCam->GetPosition();
+	ray.Direction = renderCam->GetRayDirection()[x + y * renderCam->GetViewPortWidth()] + Utils::Randomoffset1(-0.001f, 0.001f);
+
+	Hitrec hitrecord;
+
+	// Background color RGBa components
+	glm::vec4 background = black;
+
+	// Initialize the fragColor as black background
+	glm::vec4 fragColor = background;
+
+	float diffused;
+	float ambient = 0.4f;
+	glm::vec3 lightcolor{ 1.0f };
+	float attenuation = 1.0f;
+
+	for (int i = 0; i < renderScene->Bounces; i++) {
+		if (Hittable(ray, renderScene->SphereList, hitrecord) == false) {
+
+			glm::vec4 SkyColor = Utils::Lerp(ray, white, blue);
+
+			fragColor += SkyColor * attenuation;
+
+			return fragColor;
+		}
+
+		const Sphere& sphere = renderScene->SphereList[hitrecord.Hitobjindex];
+		const Material& mat = renderScene->Materials[sphere.MatIndex];
+
+		glm::vec3 Tolight(-renderScene->Lightdirection);
+		diffused = glm::max(glm::dot(hitrecord.Surfacenormal, Tolight), 0.0f);
+
+		glm::vec3 ambientcolor = mat.Albedo * ambient;
+		glm::vec3 diffusedcolor = mat.Albedo * diffused * lightcolor;
+
+
+		fragColor += glm::vec4(ambientcolor + diffusedcolor, 1.0f) * attenuation;
+
+		attenuation *= 0.35f; // 0.5f
+
+		ray.Origin = hitrecord.Hitpoint + hitrecord.Surfacenormal * 0.0001f;
+		ray.Direction = glm::reflect(ray.Direction + mat.Roughness * Utils::Randomoffset2(-1.0f, 1.0f), hitrecord.Surfacenormal);
+	}
+
+	return fragColor;
+}
+#endif // 1
