4400eb5b4354efd3554a55167eb594cea815a854
Shreyos Ghosh <shreyosghosh279@gmail.com>
2024-10-01 03:47:12 +0530

    Latest Commit: Implemented Trilinear Interpolation for smoothing out the random noise texture block patterns

--- Raether/src/Perlin.h
+++ Raether/src/Perlin.h
@@ -19,11 +19,29 @@ public:
 
 	double Noise(const glm::vec3& point) const {
 
-		int i = int(glm::floor(point.x)) & (TileWidth - 1);
-		int j = int(glm::floor(point.y)) & (TileWidth - 1);
-		int k = int(glm::floor(point.z)) & (TileWidth - 1);
+		double u = point.x - glm::floor(point.x);
+		double v = point.y - glm::floor(point.y);
+		double w = point.z - glm::floor(point.z);
 
-		return RandomDoubles[PermuteX[i] ^ PermuteY[j] ^ PermuteZ[k]];
+		int i = int(glm::floor(point.x));
+		int j = int(glm::floor(point.y));
+		int k = int(glm::floor(point.z));
+
+		double Lerp[2][2][2];
+
+		for (int di = 0; di < 2; di++) {
+			for (int dj = 0; dj < 2; dj++) {
+				for (int dk = 0; dk < 2; dk++) {
+					Lerp[di][dj][dk] = RandomDoubles[
+						PermuteX[(i + di) & (TileWidth - 1)] ^
+							PermuteY[(j + dj) & (TileWidth - 1)] ^
+							PermuteZ[(k + dk) & (TileWidth - 1)]
+					];
+				}
+			}
+		}
+
+		return TriLerp(&Lerp[0][0][0], 2, u, v, w);
 	}
 
 private:
@@ -41,6 +59,23 @@ private:
 			p[i] = p[target];
 			p[target] = temp;
 		}
+	}
+	static double TriLerp(const double* lerp, int size, double u, double v, double w) {
+		double Accum = 0.0;
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				for (int k = 0; k < size; k++) {
+					Accum +=
+						(i * u + (1 - i) * (1 - u)) *
+						(j * v + (1 - j) * (1 - v)) *
+						(k * w + (1 - k) * (1 - w)) *
+						lerp[i * size * size + j * size + k];
+				}
+			}
+		}
+
+		return Accum;
 	}
 
 private:
