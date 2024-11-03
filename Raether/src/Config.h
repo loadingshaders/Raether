6ee2620b6f6b488b913cfbe3d0caebe116b6f995
Shreyos Ghosh <shreyosghosh279@gmail.com>
2024-11-03 14:12:16 +0530

    Latest Commit: Enabled the 'SCENE10'

--- Raether/src/Config.h
+++ Raether/src/Config.h
@@ -21,7 +21,7 @@
 #define RBVH 0 // Enable Random Axis BVH Split (The Default is Longest Axis Split)
 
 // Scene Related Macros
-#define SCENE9 // Enable a Particular Scene
+#define SCENE10 // Enable a Particular Scene
 #define NumofProcSpheres 40 // Represents Total Number of Procedural Spheres
 
 // Logging Related Macros
