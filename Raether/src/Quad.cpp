3a074754759c754828772ab537b581f237fe8e4f
Shreyos Ghosh <shreyosghosh279@gmail.com>
2024-11-06 01:08:34 +0530

    Latest Commit: Added explicit type conversion to the 'Normal' from 'glm::dvec3' to 'glm::vec3'

--- Raether/src/Quad.cpp
+++ Raether/src/Quad.cpp
@@ -57,7 +57,7 @@ bool Quad::Hit(const Ray& ray, Hitrec& hitrecord) const {
 
 	/// Case-4: Ray hits the 2D shape; set the rest of the hit record and return true
 	hitrecord.HitPoint = glm::vec3(hitPoint);
-	hitrecord.SurfaceNormal = Normal;
+	hitrecord.SurfaceNormal = glm::vec3(Normal);
 	hitrecord.SetFrontFace(ray.Direction, hitrecord.SurfaceNormal);
 	hitrecord.MatId = MaterialId;
 	hitrecord.ClosestHit = t;
