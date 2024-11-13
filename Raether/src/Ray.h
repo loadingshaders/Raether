378d1b87601af7b963c850d8c2058260f487ef40
Shreyos Ghosh <shreyosghosh279@gmail.com>
2024-11-14 03:44:54 +0530

    Latest Commit: Converted the 'Ray' from 'struct' to 'class' and added the constructors

--- Raether/src/Ray.h
+++ Raether/src/Ray.h
@@ -2,12 +2,25 @@
 
 #include <glm\glm.hpp>
 
-struct Ray {
-	glm::vec3 Origin;
-	glm::vec3 Direction;
-	float Time;
+class Ray {
+public:
+	Ray() :
+		Origin(glm::vec3(0.f)),
+		Direction(glm::vec3(0.f, 0.f, -1.f)),
+		Time(0.f)
+	{}
+	Ray(const glm::vec3& origin, const glm::vec3& direction, const float& time) :
+		Origin(origin),
+		Direction(direction),
+		Time(time)
+	{}
 
 	const float GetTime() const {
 		return Time;
 	}
+
+public:
+	glm::vec3 Origin;
+	glm::vec3 Direction;
+	float Time;
 };
