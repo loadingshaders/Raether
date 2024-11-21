fc8fe27b39c650502f332fb38416e386fee7e306
Shreyos Ghosh <shreyosghosh279@gmail.com>
2024-11-22 01:42:17 +0530

    Latest Commit: Added a tiny offset to the hitpoint towards the outer surface normal

--- Raether/src/Sphere.cpp
+++ Raether/src/Sphere.cpp
@@ -72,9 +72,9 @@ bool Sphere::Hit(const Ray& ray, Interval hitdist, Hitrec& hitrecord) const {
 	/// Case-3: Ray hits the Sphere; set the rest of the hit record and return true
 	hitrecord.ClosestHit = nearHit;
 	hitrecord.HitPoint = ray.Origin + (float)hitrecord.ClosestHit * ray.Direction;
-	hitrecord.HitPoint += hitrecord.SurfaceNormal * 0.00001f;
 	hitrecord.SurfaceNormal = (hitrecord.HitPoint - glm::vec3(sphereOrigin)) / Radius;
 	hitrecord.SetFrontFace(ray.Direction, hitrecord.SurfaceNormal);
+	hitrecord.HitPoint += hitrecord.SurfaceNormal * 0.000001f;
 	GetSphereUV(hitrecord.SurfaceNormal, hitrecord.U, hitrecord.V);
 	hitrecord.MatId = MaterialId;
 
