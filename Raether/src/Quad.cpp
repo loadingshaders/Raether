51c1ecfe2cee611f4439a2e5ff0261857f54ff36
Shreyos Ghosh <shreyosghosh279@gmail.com>
2024-11-22 01:43:45 +0530

    Latest Commit: Added a tiny offset to the hitpoint towards the outer surface normal and increased the precision for the case when the ray is parallel to the plane

--- Raether/src/Quad.cpp
+++ Raether/src/Quad.cpp
@@ -42,11 +42,11 @@ bool Quad::Hit(const Ray& ray, Interval hitdist, Hitrec& hitrecord) const {
 	double Denom = glm::dot(Normal, glm::dvec3(ray.Direction));
 
 	/// Case-1: The Ray is Parallel to the Plane
-	if (std::fabs(Denom) < 1e-7) return false;
+	if (std::fabs(Denom) < 1e-8) return false;
 
 	/// Case-2: If the Ray Hit Distance is outside the Ray Interval
 	double t = (D - glm::dot(Normal, glm::dvec3(ray.Origin))) / Denom;
-	
+
 	if (!hitdist.Contains(t)) return false;
 
 	/// Case-3: Check If the Ray Lies within the Planar Shape using it's Planar Co-ordinates
@@ -63,7 +63,7 @@ bool Quad::Hit(const Ray& ray, Interval hitdist, Hitrec& hitrecord) const {
 	hitrecord.HitPoint = glm::vec3(hitPoint);
 	hitrecord.SurfaceNormal = glm::vec3(Normal);
 	hitrecord.SetFrontFace(ray.Direction, hitrecord.SurfaceNormal);
-	hitrecord.HitPoint += hitrecord.SurfaceNormal * 0.00001f;
+	hitrecord.HitPoint += hitrecord.SurfaceNormal * 0.000001f;
 	hitrecord.MatId = MaterialId;
 
 	return true;
